77f91b1acbc8fcc2162228d4d03815fb2241c8cc
bhaskar <bhaskarpotala@gmail.com>
2023-05-23 22:34:08 +0530

    bug fix

--- src/UI/util.ts
+++ src/UI/util.ts
@@ -7,3 +7,13 @@ export const modifyTableData = ({ data }: any) => {
     return { ...record, isCompleted: record.isCompleted.toString() };
   });
 };
+
+export const convertTableData = ({ data }: any) => {
+  if (!data || !data.length) {
+    return [];
+  }
+
+  return data.map((record: any) => {
+    return { ...record, isCompleted: record.isCompleted === 'true', timesSolved: +record.timesSolved };
+  });
+};
